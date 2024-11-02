75bbae3ac82ef1d13c7c98a2a1f295755fbfce5e
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-11-02 17:09:20 +0100

    feat(toolkit): Create the os_info_discord script

--- os_configure.zsh
+++ os_configure.zsh
@@ -4,6 +4,6 @@ export os_toolkit_path=${os_toolkit_path:-${0:A:h}}
 
 # Toolkit
 export FPATH="$FPATH:$os_toolkit_path/toolkit"
-autoload -U os_info os_info_telegram os_warning os_menu os_require os_kotlin
+autoload -U os_info os_info_telegram os_info_discord os_warning os_menu os_require os_kotlin
 
 export os_toolkit_import="$os_toolkit_path/os_configure"
